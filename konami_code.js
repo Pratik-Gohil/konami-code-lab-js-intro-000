6b348ea9272dec0b60d9f1ae8dffd917bc712ace
Pratik <pr.tikgohil@gmail.com>
2020-02-26 10:05:03 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,5 +14,8 @@ const codes = [
 function init() {
   let pressed = []
   // your code here
-  document.body.addEventListener("keydown", (event) => {})
+  document.body.addEventListener("keydown", (event) => {
+    pressed.push(event.key)
+    // pressed.slice(-10-1,pressed.length-10)
+  })
 }
