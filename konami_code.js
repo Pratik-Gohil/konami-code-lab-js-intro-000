451d2e01cfc6c3a8ee4c76edbc581d1283c2ff1f
Pratik <pr.tikgohil@gmail.com>
2020-02-26 10:02:37 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,6 +17,7 @@ function init() {
   document.body.addEventListener("keydown", (event) => {
     pressed.push(event.key)
     pressed.slice(-10-1,pressed.length-10)
+    console.log(pressed)
   }
   })
 }
